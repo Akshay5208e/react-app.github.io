e77750703420834cc51820050a2f93182607f377
Akshay5208e <66344375+Akshay5208e@users.noreply.github.com>
2020-07-23 13:28:54 +0530

    Add files via upload
    mern

---
+++ mernstack/client/build/precache-manifest.c2350dd4f2968cfe8a6401829e048cae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93bf760e54bd20bb0c4fd48b4b1461d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3a26aafb6538bb1f832",
+    "url": "/static/css/2.a1251ad2.chunk.css"
+  },
+  {
+    "revision": "df2e5748c357b1cc23f8",
+    "url": "/static/css/main.ebfdaf02.chunk.css"
+  },
+  {
+    "revision": "f3a26aafb6538bb1f832",
+    "url": "/static/js/2.964a87a3.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.964a87a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2e5748c357b1cc23f8",
+    "url": "/static/js/main.ce25c146.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
---
+++ mernstack/server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mongoURI:'mongodb+srv://pegasus_01234:Qwerty60@cluster0.j9huj.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
